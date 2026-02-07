9b513db0453352c5a9f03e8c6dacad0ef95d5db7
Vladislav <vladikhunter@yandex.ru>
2026-02-07 21:49:23 +0300

    Add auth service modules (config, database, handlers, service, repository, moidels, migrations)

---
+++ auth-service/internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppPort string
+
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+
+	JWTSecret string
+}
+
+func Load() *Config {
+	_ = godotenv.Load("../../.env")
+
+	cfg := &Config{
+		AppPort: getEnv("APP_PORT", "8080"),
+
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", "postgres"),
+
+		JWTSecret: getEnv("JWT_SECRET", "dev_secret"),
+	}
+
+	return cfg
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
