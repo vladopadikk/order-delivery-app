fe18df3d880be1791ec888bbd526e6593d416e0a
Vladislav <vladikhunter@yandex.ru>
2026-02-08 16:10:58 +0300

    Add producer module

---
+++ orders-service/internal/producer.go/producer.go
@@ -0,0 +1,54 @@
+package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type OrderCreatedEvent struct {
+	OrderID    int64
+	UserID     int64
+	TotalPrice float64
+	Status     string
+}
+
+type Producer struct {
+	writer *kafka.Writer
+}
+
+func NewProducer(broker string) *Producer {
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{broker},
+		Topic:   OrderCreatedTopic,
+	})
+
+	return &Producer{writer}
+}
+
+func (p *Producer) Close() {
+	p.writer.Close()
+}
+
+func (p *Producer) PublishOrderCreated(ctx context.Context, event OrderCreatedEvent) error {
+	payload, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("marshal event: %w", err)
+	}
+
+	err = p.writer.WriteMessages(ctx,
+		kafka.Message{
+			Key:   []byte(strconv.FormatInt(event.OrderID, 10)),
+			Value: payload,
+		},
+	)
+
+	if err != nil {
+		return fmt.Errorf("write kafka message: %w", err)
+	}
+
+	return err
+}
