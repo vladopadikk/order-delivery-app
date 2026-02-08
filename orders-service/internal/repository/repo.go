ce5ec58d55e57264c87f4039ff582acf6f4ec80c
Vladislav <vladikhunter@yandex.ru>
2026-02-08 12:56:41 +0300

    Add create method in repo.go

--- orders-service/internal/repository/repo.go
+++ orders-service/internal/repository/repo.go
@@ -1,6 +1,11 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+
+	"github.com/vladopadikk/order-delivery-app/orders-service/internal/models"
+)
 
 type Repository struct {
 	db *sql.DB
@@ -8,4 +13,32 @@ type Repository struct {
 
 func NewRepository(db *sql.DB) *Repository {
 	return &Repository{db}
+}
+
+func (r *Repository) Create(ctx context.Context, userID int64, status string, totalPrice float64, deliveryAddress string) (models.Order, error) {
+	query := `
+		INSERT INTO orders (user_id, status, total_price, delivery_address) 
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, user_id, status, total_price, delivery_address, created_at
+	`
+
+	var order models.Order
+
+	err := r.db.QueryRowContext(
+		ctx,
+		query,
+		userID,
+		status,
+		totalPrice,
+		deliveryAddress,
+	).Scan(
+		&order.ID,
+		&order.UserID,
+		&order.Status,
+		&order.TotalPrice,
+		&order.DeliveryAddress,
+		&order.CreatedAt,
+	)
+
+	return order, err
 }
