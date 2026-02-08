c489d6055998b38185f58a7fad02779b005be6b5
Vladislav <vladikhunter@yandex.ru>
2026-02-08 12:29:08 +0300

    Add order entitys and order DTO in models

--- orders-service/internal/models/order.go
+++ orders-service/internal/models/order.go
@@ -1,1 +1,30 @@
 package models
+
+import "time"
+
+const (
+	StatusNew                = "NEW"
+	StatusPaymentPending     = "PAYMENT_PENDING"
+	StatusPaid               = "PAID"
+	StatusDeliveryInProgress = "DELIVERY_IN_PROGRESS"
+	StatusDelivered          = "DELIVERED"
+	StatusFailed             = "FAILED"
+	StatusCancelled          = "CANCELLED"
+)
+
+type Order struct {
+	ID              int64
+	UserID          int64
+	Status          string
+	TotalPrice      float64
+	DeliveryAddress string
+	CreatedAt       time.Time
+}
+
+type OrderItem struct {
+	ID        int64
+	OrderID   int64
+	ProductID int64
+	Quantity  uint32
+	Price     float64
+}
--- orders-service/internal/models/orderDTO.go
+++ orders-service/internal/models/orderDTO.go
@@ -1,1 +1,16 @@
 package models
+
+type OrderItemInput struct {
+	ProductID int64  `json:"item_id"`
+	Quantity  uint32 `json:"quantity"`
+}
+
+type OrderInput struct {
+	Items           []OrderItemInput `json:"items"`
+	DeliveryAddress string           `json:"delivery_address"`
+}
+
+type OrderResponse struct {
+	OrderID int64  `json:"order_id"`
+	Status  string `json:"status"`
+}
